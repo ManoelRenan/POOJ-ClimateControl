f04f45a44a079705c11fb0d19b095c867010bc8a
MarcosMoura <marcos1.moura6@gmail.com>
2023-10-14 12:01:59 -0400

    Adicionei as telas de PrimeirLogin, TelaEsqueciSenha, TelaControleDispositivos e TelaPrincipalDosDispositivos. Tambem fiz algumas alteraçoes nas telas já existentes anteriormente.

---
+++ app/src/main/java/com/example/pooj_climatecontrol/TelaControleDispositivos.java
@@ -0,0 +1,14 @@
+package com.example.pooj_climatecontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaControleDispositivos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_controle_dispositivos);
+    }
+}
---
+++ app/src/main/java/com/example/pooj_climatecontrol/TelaPrimeiroLogin.java
@@ -0,0 +1,14 @@
+package com.example.pooj_climatecontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaPrimeiroLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_primeiro_login);
+    }
+}
