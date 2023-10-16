93fa1848b2e0c227a8ce405a75322c65079591fc
MarcosMoura <marcos1.moura6@gmail.com>
2023-10-16 15:28:23 -0400

    Adiconei a navegacao da tela de login para tela principal se usuario/senha corretos e navegacao da tela de login para tela de registrar-se.

--- app/src/main/java/com/example/pooj_climatecontrol/FormLogin.java
+++ app/src/main/java/com/example/pooj_climatecontrol/FormLogin.java
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormLogin extends AppCompatActivity {
+
+    EditText usuario;
+    EditText senha;
+    Button entrar;
 
     private TextView text_tela_registrar;
 
@@ -15,6 +22,27 @@ public class FormLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_from_login);
+
+        //Evento de login --> para tela inicial
+        usuario = (EditText) findViewById(R.id.edit_email);
+        senha = (EditText) findViewById(R.id.edit_senha);
+        entrar = (Button) findViewById(R.id.btn_entrar);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ("admim".equals(usuario.getText().toString()) && "1234".equals(senha.getText().toString()))
+                {
+                    startActivity(new Intent(FormLogin.this,TelaPrimeiroLogin.class));
+
+                } else {
+                    Toast falhaAutencacao = Toast.makeText(FormLogin.this,getResources().getString(R.string.activity_main_falha_autenticacao), Toast.LENGTH_SHORT);
+                    falhaAutencacao.show();
+                }
+            }
+        });
+
+        //Evento de clique na opcao Registra-se
         IniciarComponentes();
 
         text_tela_registrar.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +54,11 @@ public class FormLogin extends AppCompatActivity {
         });
     }
 
+    protected void onSart(){
+        super.onStart();
+        usuario.setText("");
+        senha.setText("");
+    }
     private void IniciarComponentes(){
         text_tela_registrar = findViewById(R.id.text_tela_registrar);
     }
